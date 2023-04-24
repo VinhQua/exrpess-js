846ed93746a7e28bb7202de7b09282d962fc44c4
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-24 20:38:38 +0900

    Post

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express')
-const logger = require('./logger')
-const authorize = require('./authorize')
+const {people} = require('./data')
 const app = express()
 
-// req => middleware => res
-// app.use([logger,authorize])
-// app.use(express.static('./public'))
-app.get('/',(req, res) => {
-    res.send('Home')
+// static assets
+app.use(express.static('./methods-public'))
+//parse form data
+app.use(express.urlencoded({ extended: false }))
+//parse json data
+app.use(express.json())
+
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people})
 })
-app.get('/about', (req, res)=>{
-    res.send('About')
+
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body
+    if (!name) {
+        return res.status(404).json({success:false,msg:'please enter a name'})
+    }
+    res.status(201).json({success:true,person:name})
 })
-app.get('/api/products',[logger,authorize] ,(req, res)=>{
-    console.log(req.user)
-    res.send('products')
-});
+
+app.post('/login',(req,res)=>{
+    console.log(req.body)
+    const {name} = req.body
+    if (name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide credentials')
+})
 app.listen(5000,()=>{
     console.log('listening on port 5000');
 })
