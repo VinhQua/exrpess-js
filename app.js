b28df3fe082477b9b4c9d16e32d0691a2f142029
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-24 15:23:24 +0900

    App

--- app.js
+++ app.js
@@ -2,8 +2,36 @@ const express = require('express')
 const app = express()
 const {products,people} = require('./data')
 
-app.get('/',(req,res)=>{
-    res.json(products)
+
+app.get('/',(req,res) => {
+    res.send('<h1>Welcome!</h1><a href ="/api/products">Product</a>')
+})
+app.get('/api/products',(req,res)=>{
+   const newProducts = products.map(product =>{
+    const {id,name,image} = product
+    return {id,name,image}
+   })
+   res.json(newProducts)
+})
+app.get('/api/products/:id',(req,res)=>{
+    const id = req.params.id - 1
+    res.json(products[id])
+})
+app.get('/api/v1/query',(req,res)=>{
+    let sortedProducts = [...products]
+    const {search, limit} = req.query
+    if (search){
+        sortedProducts = sortedProducts.filter(product => product.name.startsWith(search))
+    }
+    if (limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if (sortedProducts.length > 1){
+        res.status(200).json(sortedProducts)
+    } else {
+        res.status(200).send('no products matched your search')
+    }
+    
 })
 
 app.listen(5000,()=>{
