faa0d4bc748e7a58fa40ca075d3e5d7fab6ba31c
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-04-24 10:23:45 +0900

    update

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const express = require('express');
+const express = require('express')
 const app = express()
+const {products,people} = require('./data')
 
-app.get('/', (req, res) => {
-    res.send('home page')
-})
-
-app.all('*', (req, res) => {
-    res.send('<h1>404 Page Not Found</h1>')
+app.get('/',(req,res)=>{
+    res.json(products)
 })
 
 app.listen(5000,()=>{
     console.log('listening on port 5000');
 })
-//app.get
-//app.post
-//app.put
-//
